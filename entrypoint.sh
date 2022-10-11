fc90ec888fe73432a29c1ea5440178dacb5eda89
peterlulu666 <49325325+peterlulu666@users.noreply.github.com>
2022-10-10 20:42:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-rclone version
-mkdir /.config
-mkdir /.config/rclone/
-wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
-rclone version
-rclone version
-rclone listremotes > remotes.txt
-service aria2 start
 jupyter lab --ip=0.0.0.0 --port=$PORT --ServerApp.token='' --ServerApp.password='' --allow-root
